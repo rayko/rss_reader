e2f53902553e624a4b9e6909ea8898f41c90e4fa
Rayko <rayko.drg@gmail.com>
2013-08-10 00:03:56 -0300

    First Commit, project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReader::Application.config.secret_token = '8f78c368dddb3fd507cb77f8545f5d9a0c82592ac86b06a3caae225279cf6813f5f23b5226b80737aba1a095809cd9052ba8eccb593b70ae84ebc0e596797a74'
