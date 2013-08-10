ce6032b0fc4974a60a8ef90d7c3e0f294f34380c
Rayko <rayko.drg@gmail.com>
2013-08-10 02:18:52 -0300

    Created user views, added user fields to db and updated gitignore to ignore temp files from editors

---
+++ db/migrate/20130810050048_add_user_data_to_users.rb
@@ -0,0 +1,7 @@
+class AddUserDataToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :login, :string
+  end
+end
