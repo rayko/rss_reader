9016751c1065e4063b5d62ad78d527b6e7c284e0
Rayko <rayko.drg@gmail.com>
2013-08-14 01:09:12 -0300

    Implemented article links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    @articles = Article.all
-    @channels = current_user.channels
-    @channel = Channel.new
-    @channel_path = user_channels_path
+    @articles = []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ RssReader::Application.routes.draw do
     resources :channels do
       get 'articles_list' => 'articles#articles_list'
       get 'articles/:id/mark_as_read' => 'articles#mark_as_read'
+      get 'articles/mark_all' => 'articles#mark_all'
     end
   end
 
