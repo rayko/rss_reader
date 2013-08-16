42f120e95b1efa24e81d66957c3bfe7a3da7714b
Rayko <rayko.drg@gmail.com>
2013-08-16 17:03:54 -0300

    Fixed bugs regarding feed manager implementation

--- lib/custom_validators/channel_valid_feed_validator.rb
+++ lib/custom_validators/channel_valid_feed_validator.rb
@@ -1,6 +1,6 @@
 class ValidFeedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    feed = FeedManager.new.get_items record.url
+    feed = FeedManager.instance.get_feed record.url
     unless feed.valid?
       record.errors[attribute] << (options[:message] || 'invalid feed')
     end
