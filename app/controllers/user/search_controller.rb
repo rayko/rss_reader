c0515145b76bea11ec2ff28fe8530f223487a682
Rayko <rayko.drg@gmail.com>
2013-08-19 08:07:00 -0300

    Added pagination on search

--- app/controllers/user/search_controller.rb
+++ app/controllers/user/search_controller.rb
@@ -2,13 +2,14 @@ class User::SearchController < ApplicationController
   before_filter :authenticate_user!
 
   def search
-    @articles = Article.search params[:search][:query]
+    @articles = Article.search params[:search][:query], :page => params[:page]
     unless params[:search][:channel_id].blank?
-      @articles.search params[:search][:query], :with => { :channel_id => params[:search][:channel_id].to_i }
+      @articles.search params[:search][:query], :with => { :channel_id => params[:search][:channel_id].to_i }, :page => params[:page]
     else
-      @articles.search params[:search][:query]
+      @articles.search params[:search][:query], :page => params[:page]
     end
-    @custom_title = "Results for #{params[:search][:query]}: #{@articles.size} items"
+    @enable_pagination = true
+    @custom_title = "Results for #{params[:search][:query]}: #{@articles.total_entries} items"
     respond_to do |format|
       format.html { }
     end
