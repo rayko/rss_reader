73b2a01a2825ba557c16639877a084a4ca48010b
Rayko <rayko.drg@gmail.com>
2013-08-19 05:46:11 -0300

    Updated users views to twitter bootstrap design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_user_presence
   protect_from_forgery
 
   def index
@@ -6,12 +7,24 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       @articles_count = current_user.articles_count
       @starred_articles_count = current_user.starred_articles_count
+      @custom_title = 'Pick a channel'
+    else
+      redirect_to new_user_session_url, :layout => 'guest'
     end
-    @custom_title = 'Pick a channel'
+
   end
 
   def error_404
     flash[:alert] = 'Error 404: requested page not found.'
     redirect_to root_path
+  end
+
+  protected
+  def layout_by_user_presence
+    if devise_controller?
+      'guest'
+    else
+      'application'
+    end
   end
 end
