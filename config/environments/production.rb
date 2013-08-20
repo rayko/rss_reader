0a5f826f3139f1d61de2154ec631d6b1e301a268
Rayko <rayko.drg@gmail.com>
2013-08-20 03:14:08 -0300

    Added mailer configs for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,7 @@ RssReader::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 25 }
   config.action_mailer.default_url_options = { :host => "reader.raykosite.com" }
 end
