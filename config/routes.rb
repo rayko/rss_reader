1094f35847e3ce5774036a0558d033872debee3d
Rayko <rayko.drg@gmail.com>
2013-08-20 08:09:34 -0300

    Routes back to where they were

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 RssReader::Application.routes.draw do
-  root :to => 'application#index'
-
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   devise_for :users, :controllers => {:omniauth_callbacks => 'users/omniauth_callbacks', :registrations => 'users/registrations' }
 
@@ -26,7 +23,7 @@ RssReader::Application.routes.draw do
   end
 
 
-
-
+  root :to => 'application#index'
+  ActiveAdmin.routes(self)
   match '*not_found', to: 'application#error_404' unless Rails.application.config.consider_all_requests_local
 end
