c813f65520936f2019e8396d96f773a2bfb1bab8
Rayko <rayko.drg@gmail.com>
2013-08-21 19:45:52 -0300

    Added some more tests to article features

--- features/step_definitions/article_stepdef.rb
+++ features/step_definitions/article_stepdef.rb
@@ -17,3 +17,30 @@ Then(/^I should see 1 article$/) do
   wait_ajax_calls
   all('.article').count.should eq(1)
 end
+
+When(/^I click the first article$/) do
+  wait_ajax_calls
+  all('.article.unread').first.click
+
+end
+
+Then(/^I should see 1 read article$/) do
+  wait_ajax_calls
+  all('.article').first.should_not have_css('.unread')
+end
+
+Then(/^I should see 1 read article and many unread articles$/) do
+  wait_ajax_calls
+  all('.article').first.should_not have_css('.unread')
+  all('.unread').count.should_not eq(0)
+end
+
+When(/^I click All items link within channel articles$/) do
+  within('#dynamic_content') do
+    click_link ('All items')
+  end
+end
+
+When(/^I click All items link within special channels$/) do
+  find('.full_articles_list').click
+end
