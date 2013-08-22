681a52d82521555573826ffc85ed0165e247845e
Rayko <rayko.drg@gmail.com>
2013-08-22 05:55:21 -0300

    Added search specs

--- spec/requests/user/comments_spec.rb
+++ spec/requests/user/comments_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe "Comments" do
-  describe "GET /comments" do
-    it "gets comments index" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get user_channels_url
-      pending
-      # App returns code 302 instead of 200, don't know why yet, probably because of caches
-      # response.status.should be(302)
+  before do
+    @profile_type ||= create(:profile_type)
+    sign_in_as_a_valid_user
+    @channel ||= create(:channel, :user_id => @user.id)
+    @article ||= create(:article, :channel_id => @channel.id)
+  end
+  describe "GET user/articles/1/comments" do
+    it "gets comments from article 1" do
+      get user_article_comments_url(@article)
+
+      response.status.should be(200)
+    end
+  end
+
+  describe "POST user/articles/1/comments" do
+    it "posts a comment for article 1" do
+      post user_article_comments_url(@article), 'comment[text]' => 'Some text', 'comment[article_id]' => @article.id
+
+      response.status.should be(200)
     end
   end
 end
